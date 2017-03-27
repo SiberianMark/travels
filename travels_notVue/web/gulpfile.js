ab47db1cb1bfd335a503f7e007c6602a7361dc07
JASONM <mashaobin911215@gmail.com>
2017-03-27 16:08:59 +0800

    调整样式以及增加代码压缩部署

--- travels_notVue/web/gulpfile.js
+++ travels_notVue/web/gulpfile.js
@@ -8,6 +8,8 @@ var sourcemaps=require('gulp-sourcemaps');
 //引入异常处理模块,当出现异常时不终止watch事件（gulp-plumber），并提示我们出现了错误（gulp-notify）。
 var notify=require('gulp-notify');
 var plumber = require('gulp-plumber');
+
+
 //新建任务编译多个less文件,另外生成sourcemap保证当less有各种引入关系是可以进行查找
 gulp.task('compliLess',function(){
 	return gulp.src('./less/*.less')
@@ -23,5 +25,29 @@ gulp.task('watchComLess',function(){
 	gulp.watch('./less/*.less',['compliLess']);
 });
 
-//异常处理
+
+
+
+//引入压缩js模块
+var uglify = require('gulp-uglify')
+//引入合并模块
+var concat = require('gulp-concat');
+//新建任务压缩部分js文件
+gulp.task('uglfyCommon',function(){
+	return gulp.src(['./src/js/common/jquery.min.js','./src/js/common/jquery.cookie.js','./src/js/common/doT.min.js','./src/js/common/config.js','./src/js/common/function.js'])
+		.pipe(concat('common.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./js/common'))
+});
+
+//新建任务压缩部分js文件
+gulp.task('uglfyJS',['uglfyCommon'],function(){
+	gulp.src(['./src/js/JMCalender.js','./src/js/JMDate.js','./src/js/head.js.js','./src/js/head.css.js','./src/js/head.meta.js'])
+		.pipe(uglify())
+		.pipe(gulp.dest('./js/'))
+});
+
+gulp.task('watchUgJs',function(){
+	gulp.watch('./src/js/**/*.js')
+})
 
--- travels_notVue/web/js/head.css.js
+++ travels_notVue/web/js/head.css.js
@@ -1,3 +1,1 @@
-document.writeln('<link rel="stylesheet" href="/web/css/reset.css" />');
-document.writeln('<link rel="stylesheet" href="/web/css/common.css" />');
-document.writeln('<link rel="stylesheet" href="//at.alicdn.com/t/font_w10oc17p52pdgqfr.css">');
+document.writeln('<link rel="stylesheet" href="/web/css/reset.css" />'),document.writeln('<link rel="stylesheet" href="/web/css/common.css" />'),document.writeln('<link rel="stylesheet" href="//at.alicdn.com/t/font_w10oc17p52pdgqfr.css">');
