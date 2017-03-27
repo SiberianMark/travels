b881852beea32d37e255e690f07bafa46333b183
JASONM <mashaobin911215@gmail.com>
2017-03-27 18:43:07 +0800

    调试Pro可运行

--- travlesPro/index.php
+++ travlesPro/index.php
@@ -16,8 +16,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 
-// 绑定访问Admin模块
-define('BIND_MODULE','Home');
+// 绑定访问Admin模块//多模块此项要删除
+// define('BIND_MODULE','Admin');
+ // define('BIND_MODULE','Home');
 define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./Application/');
