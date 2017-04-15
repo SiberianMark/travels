46924e33af8fad213ca14e2f434290bae75df5c0
JASONM <mashaobin911215@gmail.com>
2017-04-15 18:20:39 +0800

    Alert project Directory Structure

---
+++ travels_notVue/Application/Admin/Model/AdminModel.class.php
@@ -0,0 +1,60 @@
+<?php
+namespace Admin\Model;
+
+use Think\Model;
+
+class AdminModel extends Model
+{
+	public $trueTableName = 'mmm_employee';
+
+	public $fields = array('id', 'name', 'phone', 'password', 'department', 'qq', 'weixin', 'email', 'sex', 'nativeplace', 'address', 'entrytime', 'status', 'addtime', 'note', 'is_del', 'loginip', 'lastlogintime', '_pk' => 'id', );
+
+	/**
+	 * Single
+	 *
+	 * @return AdminModel
+	 */
+	public static function getInstance()
+	{
+		return parent::getInstance(__CLASS__);
+	}
+
+	/**
+	 * 部门： 1-OP
+	 */
+	const DEPT_OP = 1;
+
+	/**
+	 * 部门： 2-财务部
+	 */
+	const DEPT_CW = 2;
+
+
+	/**
+	 * @param string $name
+	 * @return mixed
+	 */
+	public function getUserByName($name)
+	{
+		return $this->field(true)->where(array('name' => $name, 'is_del' => 0, 'status' => 0))->find();
+	}
+
+	/**
+	 * @param string $phone
+	 * @return mixed
+	 */
+	public function getUserByPhone($phone)
+	{
+		return $this->field(true)->where(array('phone' => $phone, 'is_del' => 0, 'status' => 0))->find();
+	}
+
+
+	/**
+	 * @param int $id
+	 * @return mixed
+	 */
+	public function getUserById($id)
+	{
+		return $this->field(true)->where(array('id' => $id, 'is_del' => 0, 'status' => 0))->find();
+	}
+}
---
+++ travels_notVue/Application/Home/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'DEFAULT_THEME'         =>  'pc',	// 默认模板主题名称
+
+    
+);
